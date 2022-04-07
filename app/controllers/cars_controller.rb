e45577f03f59ba726378dc42309d4f0efd1b3a21
Clifton Butler <cliftonmbutler@gmail.com>
2022-04-06 20:52:54 -0500

    create app and add all routes

---
+++ app/controllers/cars_controller.rb
@@ -0,0 +1,41 @@
+class CarsController < ApplicationController
+
+  def index
+    cars = Car.all
+    render json: cars
+  end
+
+  def create
+    car = Car.new(
+      make: params[:input_make],
+      model: params[:input_model],
+      year: params[:input_year],
+      name: params[:input_name]
+    )
+    car.save
+    render json: car
+  end
+
+  def show
+    car = Car.find_by(id: params[:id])
+    render json: car
+  end
+
+  def update
+    car = Car.find_by(id: params[:id])
+    car.make = params[:input_make] || car.make
+    car.model = params[:input_model] || car.model
+    car.year = params[:input_year] || car.year
+    car.name = params[:input_name] || car.name
+
+    car.save
+    render json: car
+  end
+
+  def destroy
+    car = Car.find_by(id: params[:id])
+    car.delete
+    render json: {message: "Car deleted"}
+  end
+
+end
